5ed0dd020d12bf006a8400a9b3c2d9b4837decc5
Тим <flymacmail@gmail.com>
2025-08-28 03:23:01 +0300

    Update 4-доработка.sql

--- 4-доработка.sql
+++ 4-доработка.sql
@@ -1,4 +1,6 @@
--- 
+-- Дополнительный альбом для проверки доработанного варианта
+INSERT INTO Album (AlbumId, Title, "Year") VALUES(6, 'My songs)', 0);
+
 -- Дополнительные треки для проверки доработанного варианта
 INSERT INTO Track (Title, Duration, AlbumID) VALUES
 	('myself', 0, 6),
@@ -27,3 +29,15 @@ OR Title ILIKE 'my %'     /* Где слово в начале строки */
 OR Title ILIKE '% my'     /* Где слово в конце строки */
 OR Title ILIKE '% my %'   /* Где слово в середине строки */
 OR Title ILIKE 'my';       /* Где название трека состоит из одного искомого слова */
+
+/* Результат:
+|title    |
+|---------|
+|Мой путь |
+|My Demons|
+|Мой город|
+|my own   |
+|own my   |
+|my       |
+|oh my god|
+*/
